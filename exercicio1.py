a87335789ab792e0aeeacbd22ca509c7adf72446
samuel martins <samuel.nascimento@sou.inteli.edu.br>
2023-08-15 08:55:38 -0300

    ponderada semana 2

--- exercicio1.py
+++ exercicio1.py
@@ -5,7 +5,13 @@
 
 
 # Crie a função que será avaliada no exercício aqui
-
+def conta_palavras_unicas(frase):
+    # Quebra a frase em palavras e remove qualquer pontuação
+    palavras = frase.lower().split()
+    # Cria um grupo de palavras únicas usando um "set"
+    palavras_unicas = set(palavras)
+    # Retorna quantas palavras únicas encontramos
+    return len(palavras_unicas)
 
 
 
--- exercicio2.py
+++ exercicio2.py
@@ -5,7 +5,12 @@
 
 
 # Crie a função que será avaliada no exercício aqui
-
+def is_anagram(string1, string2):
+    # Convertendo as strings para letras minúsculas e removendo espaços em branco
+    string1 = string1.lower().replace(" ", "")
+    string2 = string2.lower().replace(" ", "")
+    # Verificando se as strings são anagramas ao ordenar seus caracteres
+    return sorted(string1) == sorted(string2)
 
 
 
